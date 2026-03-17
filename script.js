f903e72a1755e96786f12554cdb7ac6e2e0a6561
Sukumar dey <dey.sukumar1999@gmail.com>
2026-03-17 21:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+import "./styles.css";
 //your JS code here. If required.
+function handelSubmit(event) {
+  event.preventDefault();
+  console.clear();
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+  let msg = "";
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (age > 18) {
+        msg = "Welcome, . You can vote.";
+      } else {
+        msg = "Oh sorry . You aren't old enough.";
+      }
+      res(msg);
+    }, 4000);
+  });
+  promise.then((value) => alert(value));
+}
+
+document.getElementById("form").addEventListener("submit", handelSubmit);
